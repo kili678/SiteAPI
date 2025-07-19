f5292432230fe82e3e46f9f01004ef137c1bc348
kili678 <70292153+kili678@users.noreply.github.com>
2025-07-19 22:38:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ data = {
 def get_owner():
     return jsonify({"owner": data["owner"]})
 
-@app.route("/owner", methods=["GET"])
+@app.route("/peches", methods=["GET"])
 def get_peches():
     peches = {k: v for k, v in data.items() if k != "owner"}
     return jsonify(peches)
